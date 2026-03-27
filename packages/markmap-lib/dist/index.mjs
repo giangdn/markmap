bd15d50503ba0b5ca05b753f5183c368fd8dba22
Ngoc Giang <giang.ngocdao@gmail.com>
2026-03-27 16:58:06 +0700

    new building

--- packages/markmap-lib/dist/index.mjs
+++ packages/markmap-lib/dist/index.mjs
@@ -1,4 +1,4 @@
-import { UrlBuilder, loadCSS, loadJS } from "markmap-common";
+import { UrlBuilder } from "markmap-common";
 import { buildTree } from "markmap-html-parser";
 import MarkdownIt from "markdown-it";
 import md_ins from "markdown-it-ins";
@@ -119,6 +119,264 @@ class Transformer {
     const keys = this.plugins.map((plugin) => plugin.name).filter((name) => features[name]);
     return this.getAssets(keys);
   }
+}
+Math.random().toString(36).slice(2, 8);
+function defer() {
+  const obj = {};
+  obj.promise = new Promise((resolve, reject) => {
+    obj.resolve = resolve;
+    obj.reject = reject;
+  });
+  return obj;
+}
+function memoize(fn) {
+  const cache = {};
+  return function memoized(...args) {
+    const key = `${args[0]}`;
+    let data = cache[key];
+    if (!data) {
+      data = {
+        value: fn(...args)
+      };
+      cache[key] = data;
+    }
+    return data.value;
+  };
+}
+/*! @gera2ld/jsx-dom v2.2.2 | ISC License */
+const VTYPE_ELEMENT = 1;
+const VTYPE_FUNCTION = 2;
+const SVG_NS = "http://www.w3.org/2000/svg";
+const XLINK_NS = "http://www.w3.org/1999/xlink";
+const NS_ATTRS = {
+  show: XLINK_NS,
+  actuate: XLINK_NS,
+  href: XLINK_NS
+};
+const isLeaf = (c) => typeof c === "string" || typeof c === "number";
+const isElement = (c) => (c == null ? void 0 : c.vtype) === VTYPE_ELEMENT;
+const isRenderFunction = (c) => (c == null ? void 0 : c.vtype) === VTYPE_FUNCTION;
+function h(type, props, ...children) {
+  props = Object.assign({}, props, {
+    children: children.length === 1 ? children[0] : children
+  });
+  return jsx(type, props);
+}
+function jsx(type, props) {
+  let vtype;
+  if (typeof type === "string") vtype = VTYPE_ELEMENT;
+  else if (typeof type === "function") vtype = VTYPE_FUNCTION;
+  else throw new Error("Invalid VNode type");
+  return {
+    vtype,
+    type,
+    props
+  };
+}
+function Fragment(props) {
+  return props.children;
+}
+const DEFAULT_ENV = {
+  isSvg: false
+};
+function insertDom(parent, nodes) {
+  if (!Array.isArray(nodes)) nodes = [nodes];
+  nodes = nodes.filter(Boolean);
+  if (nodes.length) parent.append(...nodes);
+}
+function mountAttributes(domElement, props, env) {
+  for (const key in props) {
+    if (key === "key" || key === "children" || key === "ref") continue;
+    if (key === "dangerouslySetInnerHTML") {
+      domElement.innerHTML = props[key].__html;
+    } else if (key === "innerHTML" || key === "textContent" || key === "innerText" || key === "value" && ["textarea", "select"].includes(domElement.tagName)) {
+      const value = props[key];
+      if (value != null) domElement[key] = value;
+    } else if (key.startsWith("on")) {
+      domElement[key.toLowerCase()] = props[key];
+    } else {
+      setDOMAttribute(domElement, key, props[key], env.isSvg);
+    }
+  }
+}
+const attrMap = {
+  className: "class",
+  labelFor: "for"
+};
+function setDOMAttribute(el, attr, value, isSVG) {
+  attr = attrMap[attr] || attr;
+  if (value === true) {
+    el.setAttribute(attr, "");
+  } else if (value === false) {
+    el.removeAttribute(attr);
+  } else {
+    const namespace = isSVG ? NS_ATTRS[attr] : void 0;
+    if (namespace !== void 0) {
+      el.setAttributeNS(namespace, attr, value);
+    } else {
+      el.setAttribute(attr, value);
+    }
+  }
+}
+function flatten(arr) {
+  return arr.reduce((prev, item) => prev.concat(item), []);
+}
+function mountChildren(children, env) {
+  return Array.isArray(children) ? flatten(children.map((child) => mountChildren(child, env))) : mount(children, env);
+}
+function mount(vnode, env = DEFAULT_ENV) {
+  if (vnode == null || typeof vnode === "boolean") {
+    return null;
+  }
+  if (vnode instanceof Node) {
+    return vnode;
+  }
+  if (isRenderFunction(vnode)) {
+    const {
+      type,
+      props
+    } = vnode;
+    if (type === Fragment) {
+      const node = document.createDocumentFragment();
+      if (props.children) {
+        const children = mountChildren(props.children, env);
+        insertDom(node, children);
+      }
+      return node;
+    }
+    const childVNode = type(props);
+    return mount(childVNode, env);
+  }
+  if (isLeaf(vnode)) {
+    return document.createTextNode(`${vnode}`);
+  }
+  if (isElement(vnode)) {
+    let node;
+    const {
+      type,
+      props
+    } = vnode;
+    if (!env.isSvg && type === "svg") {
+      env = Object.assign({}, env, {
+        isSvg: true
+      });
+    }
+    if (!env.isSvg) {
+      node = document.createElement(type);
+    } else {
+      node = document.createElementNS(SVG_NS, type);
+    }
+    mountAttributes(node, props, env);
+    if (props.children) {
+      let childEnv = env;
+      if (env.isSvg && type === "foreignObject") {
+        childEnv = Object.assign({}, childEnv, {
+          isSvg: false
+        });
+      }
+      const children = mountChildren(props.children, childEnv);
+      if (children != null) insertDom(node, children);
+    }
+    const {
+      ref
+    } = props;
+    if (typeof ref === "function") ref(node);
+    return node;
+  }
+  throw new Error("mount: Invalid Vnode!");
+}
+function mountDom(vnode) {
+  return mount(vnode);
+}
+function hm(...args) {
+  return mountDom(h(...args));
+}
+const memoizedPreloadJS = memoize((url) => {
+  document.head.append(
+    hm("link", {
+      rel: "preload",
+      as: "script",
+      href: url
+    })
+  );
+});
+const jsCache = {};
+const cssCache = {};
+async function loadJSItem(item, context) {
+  var _a;
+  const src = item.type === "script" && ((_a = item.data) == null ? void 0 : _a.src) || "";
+  item.loaded || (item.loaded = jsCache[src]);
+  if (!item.loaded) {
+    const deferred = defer();
+    item.loaded = deferred.promise;
+    if (item.type === "script") {
+      document.head.append(
+        hm("script", {
+          ...item.data,
+          onLoad: () => deferred.resolve(),
+          onError: deferred.reject
+        })
+      );
+      if (!src) {
+        deferred.resolve();
+      } else {
+        jsCache[src] = item.loaded;
+      }
+    }
+    if (item.type === "iife") {
+      const { fn, getParams } = item.data;
+      fn(...(getParams == null ? void 0 : getParams(context)) || []);
+      deferred.resolve();
+    }
+  }
+  await item.loaded;
+}
+async function loadCSSItem(item) {
+  const url = item.type === "stylesheet" && item.data.href || "";
+  item.loaded || (item.loaded = cssCache[url]);
+  if (!item.loaded) {
+    const deferred = defer();
+    item.loaded = deferred.promise;
+    if (url) cssCache[url] = item.loaded;
+    if (item.type === "style") {
+      document.head.append(
+        hm("style", {
+          textContent: item.data
+        })
+      );
+      deferred.resolve();
+    } else if (url) {
+      document.head.append(
+        hm("link", {
+          rel: "stylesheet",
+          ...item.data
+        })
+      );
+      fetch(url).then((res) => {
+        if (res.ok) return res.text();
+        throw res;
+      }).then(() => deferred.resolve(), deferred.reject);
+    }
+  }
+  await item.loaded;
+}
+async function loadJS(items, context) {
+  items.forEach((item) => {
+    var _a;
+    if (item.type === "script" && ((_a = item.data) == null ? void 0 : _a.src)) {
+      memoizedPreloadJS(item.data.src);
+    }
+  });
+  context = {
+    getMarkmap: () => window.markmap,
+    ...context
+  };
+  for (const item of items) {
+    await loadJSItem(item, context);
+  }
+}
+async function loadCSS(items) {
+  await Promise.all(items.map((item) => loadCSSItem(item)));
 }
 const pluginAssets = {
   katex: {
